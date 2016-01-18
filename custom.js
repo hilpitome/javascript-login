1d80ea360ba6e1790b12673c8da9cdff6e3d91e5
hilpitome <hilpitome@gmail.com>
2016-01-19 02:20:51 +0300

    finally made sign up page submit a new user

--- custom.js
+++ custom.js
@@ -1,21 +1,38 @@
 
 Parse.initialize("JJTVzrYgHH04oeM1CvAuS9cKeWQXRLQ8rQOyYnPj", "3vC8VQzkbXQM6JI96ontDh1bTTWNC9gabvKZbGal");
+// ------------------------------------------------------ SIGN UP PAGE ------- //
 
-var user = new Parse.User();
-user.set("username", "my name");
-user.set("password", "my pass");
-user.set("email", "email@example.com");
-  
-// other fields can be set just like with Parse.Object
-user.set("phone", "650-555-0000");
-  
-user.signUp(null, {
-  success: function(user) {
-    // Hooray! Let them use the app now.
-  },
-  error: function(user, error) {
-    // Show the error message somewhere and let the user try again.
-    alert("Error: " + error.code + " " + error.message);
-  }
-});
+	function myFunction() {
+		var username = $('input[name=username]').val();
+		var password = $('input[name=password]').val();
+		var email = $('input[name=email]').val();
+		var phone = $('input[name=phone]').val();
+
+		var user = new Parse.User();
+	
+		user.set("username", username);
+		user.set("email", email);
+		user.set("phone", phone);
+		user.set("password", password);
+		  
+		user.signUp(null, {
+		  success: function(user) {
+		    alert ("Hooray! Let them use the app now!!");
+		  },
+		  error: function(user, error) {
+		    // Show the error message somewhere and let the user try again.
+		    alert("Error: " + error.code + " " + error.message);
+		  }
+					
+	    });
+	}
+
+		
+
+	
+
+
+
+
+
 
